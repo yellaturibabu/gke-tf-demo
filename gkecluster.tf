b114e4bc082508e9c4a2229242b60dd8a181145e
yellaturibabu <yellaturibabu@gmail.com>
2018-11-13 00:02:03 +0530

    Update gkecluster.tf

--- gkecluster.tf
+++ gkecluster.tf
@@ -2,5 +2,5 @@ resource "google_container_cluster" "gke-cluster" {
   name               = "my-first-gke-cluster"
   network            = "default"
   zone               = "europe-west1-b"
-  initial_node_count = 3
+  initial_node_count = 2
 }
